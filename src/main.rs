85b29b7581166079b32d6700bf77fb8c09285535
Vitor Silva <vitor@silva.moe>
2020-10-28 18:43:22 -0300

    Simplify match

--- src/main.rs
+++ src/main.rs
@@ -7,40 +7,25 @@ enum Temp {
 }
 
 impl Temp {
-    fn show_all(&self) -> String {
-        let (c, f, k): (f64, f64, f64);
-
-        match self {
-            Temp::Celsius(temp) => {
-                c = *temp;
-                k = c + 273.0;
-                f = 1.8 * c + 32.0;
-            }
-            Temp::Fahrenheit(temp) => {
-                f = *temp;
-                c = (f - 32.0) / 1.8;
-                k = c + 273.0;
-                
-            }
-            Temp::Kelvin(temp) => {
-                k = *temp;
-                c = k - 273.0;
-                f = 1.8 * c + 32.0;
-            }
-        };
-
-        format!("{:.2}c {:.2}f {:.2}k", c, f, k)
+    fn get_cfk(&self) -> (f64, f64, f64) {
+        match *self {
+            Temp::Celsius(temp) => (temp, 1.8 * temp + 32.0, temp + 273.0),
+            Temp::Fahrenheit(temp) => ((temp - 32.0) / 1.8, temp, (temp - 32.0) / 1.8 + 273.0),
+            Temp::Kelvin(temp) => (temp - 273.0, 1.8 * (temp - 273.0) + 32.0, temp)
+        }
     }
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let mut args = env::args();
+    let mut input: String;
 
-    if args.len() != 2 {
-        panic!("format invalid, please use % dg N[cfk]");
+    if let None = args.next() { panic!("Some wrong is not right"); }
+    if let Some(arg) = args.next() {
+        input = arg.clone()
+    } else {
+        panic!("format input, please use % dg N[cfk]");
     }
-
-    let mut input = args[1].clone();
     
     let mode = input.pop();
     let num: f64 = input.parse().expect("err parsing number");
@@ -52,5 +37,7 @@ fn main() {
         Some(_) | None => panic!("temp must end in c, f or k"),
     };
 
-    println!("{}", temp.show_all());
+    let (c, f, k) = temp.get_cfk();
+
+    println!("{:.2}c {:.2}f {:.2}k", c, f, k);
 }
